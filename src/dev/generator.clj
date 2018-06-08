05f26c8f7425ca26aa2b2a98563d51b159b3bd67
gigasquid <cmeier@gigasquidsoftware.com>
2018-06-08 10:16:00 -0400

    add apache license to source code

--- src/dev/generator.clj
+++ src/dev/generator.clj
@@ -1,3 +1,20 @@
+;;
+;; Licensed to the Apache Software Foundation (ASF) under one or more
+;; contributor license agreements.  See the NOTICE file distributed with
+;; this work for additional information regarding copyright ownership.
+;; The ASF licenses this file to You under the Apache License, Version 2.0
+;; (the "License"); you may not use this file except in compliance with
+;; the License.  You may obtain a copy of the License at
+;;
+;;    http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+;;
+
 (ns dev.generator
   (:require [t6.from-scala.core :as $]
             [clojure.reflect :as r]
@@ -161,11 +178,31 @@
      `(~'defn ~function-name
        ~@(remove nil? (gen-symbol-function-arity op-name op-values function-name))))))
 
+(def license
+  (str
+   ";; Licensed to the Apache Software Foundation (ASF) under one or more\n"
+   ";; contributor license agreements.  See the NOTICE file distributed with\n"
+   ";; this work for additional information regarding copyright ownership.\n"
+   ";; The ASF licenses this file to You under the Apache License, Version 2.0\n"
+   ";; (the \"License\"); you may not use this file except in compliance with\n"
+   ";; the License.  You may obtain a copy of the License at\n"
+   ";;\n"
+   ";;    http://www.apache.org/licenses/LICENSE-2.0\n"
+   ";;\n"
+   ";; Unless required by applicable law or agreed to in writing, software\n"
+   ";; distributed under the License is distributed on an \"AS IS\" BASIS,\n"
+   ";; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+   ";; See the License for the specific language governing permissions and\n"
+   ";; limitations under the License.\n"
+   ";;\n"))
+
 (defn write-to-file [functions ns-gen require-import-writer-fn fname]
   (with-open [w (clojure.java.io/writer fname)]
     (.write w ns-gen)
     (.write w "\n\n")
     (.write w ";; Do not edit - this is auto-generated")
+    (.write w "\n\n")
+    (.write w license)
     (.write w "\n\n")
     (require-import-writer-fn w)
     (.write w "\n\n")
