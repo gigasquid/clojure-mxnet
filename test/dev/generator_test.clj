2384251457ed2a16fb1b4b5350b388fe806802db
gigasquid <cmeier@gigasquidsoftware.com>
2018-06-21 19:37:33 -0400

    add more test cases and filter out generated functions inner functions

--- test/dev/generator_test.clj
+++ test/dev/generator_test.clj
@@ -4,6 +4,8 @@
 
 (deftest test-clojure-case
   (is (= "foo-bar" (gen/clojure-case "FooBar")))
+  (is (= "foo-bar-baz" (gen/clojure-case "FooBarBaz")))
+  (is (= "foo-bar-baz" (gen/clojure-case "FOOBarBaz")))
   (is (= "foo-bar" (gen/clojure-case "foo_bar")))
   (is (= "foo-bar" (gen/clojure-case "Foo_Bar")))
   (is (= "div+" (gen/clojure-case "/+"))))
